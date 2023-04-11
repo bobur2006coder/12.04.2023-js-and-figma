2bc47694fd48e29e7ba06cae4811e927d66cd6df
bobur2006coder <boburdilmurodov702@gmail.com>
2023-04-11 17:40:37 +0500

    fix

---
+++ javascript.js
@@ -0,0 +1,42 @@
+let parametr = document.querySelector(".list-group")
+
+let btn = document.querySelector('.btn')
+
+let btntwo = document.querySelector(".btn-danger")
+
+btn.onclick = () => {
+parametr.classList.contains("action")
+        parametr.classList.remove("action")
+        parametr.style.transition ="1.5s";
+        btn.classList.add("visually-hidden");
+        btntwo.classList.remove("visually-hidden");
+     
+
+}
+
+btntwo.onclick = () => {
+    parametr.classList.add("action")
+    parametr.style.transition ="1.5s";
+
+    btntwo.classList.add("visually-hidden")
+        btn.classList.remove("visually-hidden")
+    
+
+}
+
+
+
+
+// bitta tugmada bajarilishi 
+
+// btn.onclick = () => {
+//     if(parametr.classList.contains("action")){
+//         parametr.classList.remove("action")
+//         parametr.style.transition ="1.5s";
+
+//     }else{
+//         parametr.classList.add("action")
+//         parametr.style.transition ="1.5s";
+
+//     }
+// }
